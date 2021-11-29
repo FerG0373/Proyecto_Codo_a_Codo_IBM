58a463842eb9f9c4658f4e944507f4f6bbe832b3
Fernando González <fer.dg.sc@gmail.com>
2021-11-29 00:28:46 -0300

    Agregando ScrollView al quiénes somos

--- views/Vanina.js
+++ views/Vanina.js
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 import React, {useState} from 'react';
-import {View, Text, StyleSheet, ImageBackground} from 'react-native';
+import {View, Text, StyleSheet, ImageBackground, ScrollView} from 'react-native';
 import globalStyles from '../styles/globalStyles';
 import {Avatar} from 'react-native-elements';
 
@@ -10,28 +10,30 @@ const Vanina = () => {
   return (
     <>
       <ImageBackground source={imagen} resizeMode="cover" style={styles.imagen}>
-        <View>
-          <View style={styles.avatar}>
-            <Avatar
-              size="xlarge"
-              rounded
-              title="VA"
-              containerStyle={{backgroundColor: 'red'}}
-            />
-          </View>
+        <ScrollView>
+          <View>
+            <View style={styles.avatar}>
+              <Avatar
+                size="xlarge"
+                rounded
+                title="VA"
+                containerStyle={{backgroundColor: 'red'}}
+              />
+            </View>
 
-          <Text style={globalStyles.titulo}> Descripcion </Text>
-          <Text style={globalStyles.parrafo}>
-            Hola! Mi nombre completo es Vanina Armida, tengo 35 años, soy de
-            Córdoba, Argentina. Soy estudiante Desarrolladora web FullStack
-            Junior, arranqué en este mundo primero estudiando de forma
-            autodidacta y después complementado con cursos como el de Desarrollo
-            web FullStack PHP en Codo a Codo. Desde finales del 2020 estoy
-            trabajando como freelance con tecnologías como React Y Node y
-            también en relación de dependencia trabajando como Desarrolladora
-            web PHP
-          </Text>
-        </View>
+            <Text style={globalStyles.titulo}> Descripcion </Text>
+            <Text style={globalStyles.parrafo}>
+              Hola! Mi nombre completo es Vanina Armida, tengo 35 años, soy de
+              Córdoba, Argentina. Soy estudiante Desarrolladora web FullStack
+              Junior, arranqué en este mundo primero estudiando de forma
+              autodidacta y después complementado con cursos como el de Desarrollo
+              web FullStack PHP en Codo a Codo. Desde finales del 2020 estoy
+              trabajando como freelance con tecnologías como React Y Node y
+              también en relación de dependencia trabajando como Desarrolladora
+              web PHP
+            </Text>
+          </View>
+        </ScrollView>
       </ImageBackground>
     </>
   );
